1c68f4f73f03092763f7202faeda674cd8c047c7
JohnWRees <32087687+JohnWRees@users.noreply.github.com>
2017-12-28 17:01:44 -0600

    Updated Server to accept values
    See Title

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+
 var server = http.createServer(function (req, res) {
-    displayForm(res);
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        //processAllFieldsOfTheForm(req, res);
+        processFormFieldsIndividual(req, res);
+    }
 });
 
 function displayForm(res) {
@@ -13,6 +21,48 @@ function displayForm(res) {
         res.write(data);
         res.end();
     });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields,
+            files: files
+        }));
+    });
+}
+
+function processFormFieldsIndividual(req, res) {
+    //Store the data from the fields in your data store.
+    //The data store could be a file or database or any other store based
+    //on your application.
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+        console.log(field);
+        console.log(value);
+        fields[field] = value;
+    });
+
+    form.on('end', function () {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields
+        }));
+    });
+    form.parse(req);
 }
 
 server.listen(1185);
