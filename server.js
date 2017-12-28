efab6f9da1c71f608da3416e6291a143db866413
JohnWRees <32087687+JohnWRees@users.noreply.github.com>
2017-12-28 17:47:58 -0600

    up7

--- server.js
+++ server.js
@@ -30,7 +30,14 @@ function processAllFieldsOfTheForm(req, res) {
         //Store the data from the fields in your data store.
         //The data store could be a file or database or any other store based
         //on your application.
-        
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields,
+            files: files
+        }));
     });
 }
 
